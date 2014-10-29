bbbdc87c112bd2a9fc80c5273dc13d07cdba4eef
student <student@r1-01.(none)>
2014-10-29 12:13:51 +0400

    Save changes

--- class-08/02-listable.hs
+++ class-08/02-listable.hs
@@ -4,9 +4,19 @@
    fromList :: [a] -> a
 -}
 
+class Listable a where
+	toList :: a -> [a]
+	fromList :: [a] -> a
+
 
 {-
   Объявите экземпляры класса типов Listable для следующих типов:
   1) String - строка разбивается по пробелам на список слов.
   2) Integral a - любое целое число разбивается на список цифр.
 -}
+
+toDigits i
+	| i == 0 = []
+	| otherwise = (i `mod` 10) : (toDigits (i `div` 10))
+
+fromDigits = foldl (\acc x -> acc * 10 + x) 0
