bc198c1ea8a85f3d5950a44e61d02d9ff8ba2d95
student <student@r1-01.(none)>
2014-11-12 12:18:07 +0400

    Begin 1

--- class-10/01-data-reader.hs
+++ class-10/01-data-reader.hs
@@ -1,3 +1,7 @@
+import Data.List
+import System.Environment
+import Data.Monoid
+
 {-
    Дан текстовый файл, содержащий данные о нескольких студентах в следующем формате: три последовательные
    строки соответствуют имени, возрасту и номеру группы (например, 4.8). Определить соответствующий тип
@@ -9,4 +13,25 @@
    блока do не допускается).
 -}
 
-main = undefined
+type Name = String
+type Age = Int
+type Group = Int
+
+data Student = Student Name Age Group
+	deriving (Show, Eq)
+
+formedStudents content = map formedStudent $ chunksOf' 3 content
+
+chunksOf' _ [] = []
+chunksOf' n content = (take n content) : (chunksOf' n $ drop n content)
+
+formedStudent (name : age : group : _) = Student name (read age :: Int) (read group :: Int)
+
+studentToList students = unlines $ map (\(Student name age group) -> unlines [name, (show age), (show group)]) students
+
+--main = (head `fmap` getArgs) >>= readFile >>= print . formedStudents . lines
+
+main = do
+	filename <- head `fmap` getArgs
+	t1 <- readFile filename
+	putStrLn $ studentToList $ formedStudents $ lines t1
